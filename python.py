5dac96d8f28a9749d3d7047b6a1acbab08cdf8ee
AdrianEriksson1 <165469890+AdrianEriksson1@users.noreply.github.com>
2024-03-30 10:45:33 +0100

    Sorting system

--- python.py
+++ python.py
@@ -1,8 +1,8 @@
-A = [5, 1, 3, 2, 4] # Program to sort an list/array, Make it!
-def sorting(list):
+A = [5, 1, 3, 2, 4] # Program to sort an list.
+def sorting(list): 
     for i in range(0, len(list) - 1):
         for u in range(i + 1, len(list)):
-            if list[u] < list[i]:
-                list[i], list[u] = list[u], list[i]
+            if list[u] < list[i]: # Check if index u in list is smaller than index i
+                list[i], list[u] = list[u], list[i] # Swaps index i and u
     print(list)           
 sorting(A)
